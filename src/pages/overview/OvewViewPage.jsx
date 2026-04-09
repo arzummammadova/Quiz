b03a733230b93722ce2600447613c17555259b04
Arzu Mammadova <arzuam-af106@code.edu.az>
2026-04-09 12:06:40 +0400

    .

--- src/pages/overview/OvewViewPage.jsx
+++ src/pages/overview/OvewViewPage.jsx
@@ -1,15 +1,48 @@
-import { Card } from 'antd'
-import React from 'react'
+import { Card, Modal,Button } from 'antd'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const OvewViewPage = () => {
+  const [isModalOpen,setIsModalOpen]=useState(false);
+    const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+
+  const [category,setCategory]=useState("")
+
+  useEffect(()=>{
+    
+    const fetchCategory=async()=>{
+      const res=await axios.get(`${import.meta.env.VITE_BASE_URL}/api/quiz/topics/${category}`)
+      console.log(res.data)
+    }
+    fetchCategory()
+  },[category])
+
   return (
     <div className='mx-auto h-screen flex items-center justify-center gap-10'>
-      <Card className='w-[500px] p-10 rounded-3xl shadow-lg  text-2xl font-bold uppercase text-center cursor-pointer hover:scale-105 transition-all duration-300'>
+      <Card onClick={()=>setCategory("backend")} className='w-[500px] p-10 rounded-3xl shadow-lg  text-2xl font-bold uppercase text-center cursor-pointer hover:scale-105 transition-all duration-300'>
         BackEnd
       </Card>
-      <Card className='w-[500px] p-10 rounded-3xl shadow-lg text-2xl font-bold  uppercase text-center cursor-pointer hover:scale-105 transition-all duration-300'>
+      <Card  onClick={()=>setCategory("frontend")} className='w-[500px] p-10 rounded-3xl shadow-lg text-2xl font-bold  uppercase text-center cursor-pointer hover:scale-105 transition-all duration-300'>
         FrontEnd
       </Card>
+       
+ <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal>
+        salam
+      </Modal>
     </div>
   )
 }
